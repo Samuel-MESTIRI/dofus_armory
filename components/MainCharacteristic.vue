f39d04686ac478ebc30a59503625c0032857317d
Samuel-MESTIRI <samuelmestiri@hotmail.fr>
2020-03-12 23:43:30 +0100

    add carac icon WIP

--- components/MainCharacteristic.vue
+++ components/MainCharacteristic.vue
@@ -2,6 +2,7 @@
   <section class="col-2 offset-1 box mr-2 mt-4 p-4">
     <article>
       <h3 class="h5">Caractérisque principale</h3>
+      <div class="icon-carac neutre"></div>
     </article>
   </section>
 </template>
